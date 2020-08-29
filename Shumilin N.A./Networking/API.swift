f533723720a0493cd5bebd9ad2e2a6bab9242915
Nikita Shumilin <firenesfall@gmail.com>
2020-08-29 20:38:52 +0300

    Add API enum to save all parts of urls.

---
+++ Shumilin N.A./Networking/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  Shumilin N.A.
+//
+//  Created by Nikita Shumilin on 29.08.2020.
+//  Copyright © 2020 Nikita Shumilin. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    static let token = "pk_8e1b66cb02504229a49c6c710a9a5c25"
+    static let baseURL = "https://cloud.iexapis.com/stable"
+}
