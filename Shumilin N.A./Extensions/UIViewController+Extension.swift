13d096a67483ed3d017186285e2e511889905b96
Nikita Shumilin <firenesfall@gmail.com>
2020-08-30 17:45:56 +0300

    Add extension methods to add and remove Child in View Controller.

---
+++ Shumilin N.A./Extensions/UIViewController+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Extension.swift
+//  Shumilin N.A.
+//
+//  Created by Nikita Shumilin on 30.08.2020.
+//  Copyright © 2020 Nikita Shumilin. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func add(child: UIViewController, to container: UIView) {
+        self.addChild(child)
+        child.view.frame = container.bounds
+        child.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        container.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    
+    func remove(child: UIViewController) {
+        child.willMove(toParent: nil)
+        child.view.removeFromSuperview()
+        child.removeFromParent()
+    }
+}
